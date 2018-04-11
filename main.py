75dfe63932e39115f4f01e91f890865cb5134cd4
MasterGowen <mastergowen@gmail.com>
2018-04-11 09:58:56 +0500

    firefix

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome("C:\\Users\\User\\Desktop\\WORKING\\chromedriver_new.exe")
+driver = webdriver.Firefox()
 
 driver.get("https://openprofession.ru/")
