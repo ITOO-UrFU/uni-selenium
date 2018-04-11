98dcac5bb865b91dc08d009bb29d99e688a2e02a
MasterGowen <mastergowen@gmail.com>
2018-04-11 10:10:03 +0500

    funcs

--- main.py
+++ main.py
@@ -4,5 +4,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
+driver.implicitly_wait(1)
 
-driver.get("uni.urfu.ru")
+
+def wait_by_id(el_id, seconds=10):
+    element = WebDriverWait(driver, seconds).until(
+        EC.presence_of_element_located((By.ID, el_id))
+    )
+    return element
+
+
+def wait_by_classname(el_class, seconds=10):
+    element = WebDriverWait(driver, seconds).until(
+        EC.presence_of_element_located((By.CLASS_NAME, el_class))
+    )
+    return element
+
+
+def wait_by_css(selector, seconds=10):
+    elements = WebDriverWait(driver, seconds).until(
+        EC.presence_of_element_located((By.CLASS_NAME, selector))
+    )
+    return elements
+
+
+driver.get("https://uni.urfu.ru/fx/")
