02675262021695d4aa56a170fff487c440e9dc95
MasterGowen <mastergowen@gmail.com>
2018-05-30 18:00:32 +0500

    click

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
+driver = webdriver.Firefox()
 # driver = webdriver.Firefox(executable_path='C:\\PROJECTS\\uni-selenium\\geckodriver.exe')
-# driver.implicitly_wait(1)
+driver.implicitly_wait(1)
 
 
 def wait_by_id(el_id, seconds=10):
@@ -34,6 +35,7 @@ def wait_by_css(selector, seconds=10):
     )
     return elements
 
+
 def wait_by_name(selector, seconds=10):
     elements = WebDriverWait(driver, seconds).until(
         EC.presence_of_element_located((By.NAME, selector))
@@ -51,51 +53,59 @@ def get_person_list():
             for row in ws.iter_rows():
                 current_row = row[0].row
                 person = {
-                    "sex": ws["A"+str(current_row)].value,
+                    "sex": ws["A" + str(current_row)].value,
                     "first_name": ws["B" + str(current_row)].value,
                     "second_name": ws["C" + str(current_row)].value,
                     "last_name": ws["D" + str(current_row)].value,
                     "organization": ws["E" + str(current_row)].value,
                     "position": ws["F" + str(current_row)].value
                 }
-                if not None in person.values():
+                if None not in person.values():
                     if current_row > 1:
                         result.append(person)
                         print(person)
                 else:
                     print("Error: 'None' in field: ", person)
 
-# driver.get("https://uni.urfu.ru/fx/")
-# wait_by_id('login',10).send_keys('n.v.ignatchenko@urfu.ru')
-# wait_by_id('password',10).send_keys('Ye;yj,jkmitrjlf')
-# wait_by_name('LogonFormSubmit',10).click()
-# #driver.execute_script('document.querySelector(".menuitem_submain_ul").style.display = "block"')
-# #driver.find_element_by_xpath('//*[@id="pohour_leaner_groups_mi_link"]').click()
-# driver.get("https://uni.urfu.ru/fx/uni/ru.naumen.uni.published_jsp?uuid=fakeobUNI_HourpayedCoreRoot&activeComponent=ActiveGroups")
-# wait_by_id('ActiveGroups.LearnerGroups.LearnerGroup', 10).click()
-# driver.switch_to.window(driver.window_handles[1])
-# WebDriverWait(driver, 10).until(lambda d: d.title != "")
-# wait_by_id('title',10).send_keys('номер групппы')
-# time.sleep(2)
-# #wait_by_id('cancel',20).click().perform()
-# wait_by_id('cancel',20).click()
-# driver.switch_to.window(driver.window_handles[0])
-# WebDriverWait(driver, 10).until(lambda d: d.title != "")
-# wait_by_id('TitleFilter').send_keys('ДК-2017м')
-# wait_by_id('TitleFilter').send_keys(Keys.ENTER)
-# #wait_by_id('ActiveGroups.LearnerGroups',20).find_elements_by_tag_name('a')[0].click()
-# driver.get("https://uni.urfu.ru/fx/uni/ru.naumen.uni.published_jsp?parentACIDdpogrp18hc2jg0000m14hnjbioi3n5ng=ActiveGroups&parentdpogrp18hc2jg0000m14hnjbioi3n5ng=fakeobUNI_HourpayedCoreRoot&uuid=dpogrp18hc2jg0000m14hnjbioi3n5ng")
-# wait_by_id('LearnerGroupTab.Learners.AddLearnerShort').click()
-# driver.switch_to.window(driver.window_handles[1])
-# WebDriverWait(driver, 10).until(lambda d: d.title != "")
-# wait_by_id('lastName',10).send_keys('Фамилия')
-# wait_by_id('firstName',10).send_keys('Имя')
-# wait_by_id('middleName',10).send_keys('Имя')
-# wait_by_id('BirthDate',10).send_keys('08.05.2018')
-# sex = 'м'
-# if (sex == "ж"):
-#     wait_by_id('Sex_pstcim18gg3ig0000glddc6svsi80ce8', 10).click()
-# if (sex == "м"):
-#     wait_by_id('Sex_pstcim18gg3ig0000glddc6svsi80ce4', 10).click()
-# wait_by_id('placeOfEmployment',10).send_keys('Место работы')
-# wait_by_id('post',10).send_keys('Должность')
+
+driver.get("https://uni.urfu.ru/fx/")
+wait_by_id('login', 10).send_keys('n.v.ignatchenko@urfu.ru')
+wait_by_id('password', 10).send_keys('Ye;yj,jkmitrjlf')
+wait_by_name('LogonFormSubmit', 10).click()
+# driver.execute_script('document.querySelector(".menuitem_submain_ul").style.display = "block"')
+# driver.find_element_by_xpath('//*[@id="pohour_leaner_groups_mi_link"]').click()
+driver.get(
+    "https://uni.urfu.ru/fx/uni/ru.naumen.uni.published_jsp?uuid=fakeobUNI_HourpayedCoreRoot&activeComponent=ActiveGroups")
+wait_by_id('ActiveGroups.LearnerGroups.LearnerGroup', 10).click()
+driver.switch_to.window(driver.window_handles[1])
+WebDriverWait(driver, 10).until(lambda d: d.title != "")
+wait_by_id('title', 10).send_keys('номер групппы')
+time.sleep(2)
+# wait_by_id('cancel',20).click().perform()
+wait_by_id('cancel', 20).click()
+driver.switch_to.window(driver.window_handles[0])
+WebDriverWait(driver, 10).until(lambda d: d.title != "")
+wait_by_id('TitleFilter').send_keys('ДК-2017м')
+wait_by_id('TitleFilter').send_keys(Keys.ENTER)
+wait_by_id('ActiveGroups.LearnerGroups', 20).find_elements_by_tag_name('a')[0].click()
+
+# group = lambda: WebDriverWait(driver, 1).until(
+#                 EC.presence_of_element_located((By.CSS_SELECTOR, '[id*="ActiveGroups"] a'))
+#             )
+# group().click()
+
+driver.execute_script('document.querySelector(\'[id*="ActiveGroups"] tr:nth-child(2) a\').click()')
+wait_by_id('LearnerGroupTab.Learners.AddLearnerShort').click()
+driver.switch_to.window(driver.window_handles[1])
+WebDriverWait(driver, 10).until(lambda d: d.title != "")
+wait_by_id('lastName', 10).send_keys('Фамилия')
+wait_by_id('firstName', 10).send_keys('Имя')
+wait_by_id('middleName', 10).send_keys('Имя')
+# wait_by_id('BirthDate', 10).send_keys('08.05.2018')
+sex = 'м'
+if sex == "ж":
+    wait_by_id('Sex_pstcim18gg3ig0000glddc6svsi80ce8', 10).click()
+if sex == "м":
+    wait_by_id('Sex_pstcim18gg3ig0000glddc6svsi80ce4', 10).click()
+wait_by_id('placeOfEmployment', 10).send_keys('Место работы')
+wait_by_id('post', 10).send_keys('Должность')
