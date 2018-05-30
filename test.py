ee663bcb8629429b5231bab5eca5d5d8d17edb06
vesloguzov <vesloguzov@gmail.com>
2018-05-30 12:11:44 +0500

    добавил точку

--- test.py
+++ test.py
@@ -48,7 +48,7 @@ root=Tk()
 # кнопка по умолчанию
 button1 = Button()
 button1.pack()
-# кнопка с указанием родительского виджета и несколькими аргументами
+# кнопка с указанием родительского виджета и несколькими аргументами.
 button2 = Button(root, bg="red", text=u"Кликни меня!", command=button_clicked)
 button2.pack()
 root.mainloop()
